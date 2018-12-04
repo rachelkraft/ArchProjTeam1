086f6ec0774a2d5259100d9337b68f83391275d7
Rachel <rkraft@udel.edu>
2018-12-04 04:01:33 -0500

    small changes to run_shootout.sh

--- prng64-shootout/run_shootout.sh
+++ prng64-shootout/run_shootout.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-echo -n "" > output_shootout_25.txt
-for run in {1..25}
+
+#clear the output file before we append to it
+echo -n "" > output_shootout_30.txt
+
+#compile shootout.c
+gcc shootout.c -o shootout
+
+for run in {1..30}
 do
   ./shootout >> output_shootout_25.txt
-  echo "/////////////////////\n" >> output_shootout_25.txt
+  echo "/////////////////////\n" >> output_shootout_30.txt
 done
