695e39c9ad180ca56d75d58ecbdceed1e16a76d4
Rachel <rkraft@udel.edu>
2018-12-04 03:06:52 -0500

    edited run_4algs_generate_random_numbers.sh in order to go through all 4 algorithms 10x each. The results are stored in a folder called results inside the directory you call this file in. In total running this script should generate 24 files, 6 for each algorithm. 3 of these 6 are the random numbers for 1mill, 5 mill, 10 mill, and the other 3 files are the 10 time runs for generating 1mill, 5mill, and 10mill numbers respectively

--- prng64-shootout/run_4algs_generate_random_numbers.sh
+++ prng64-shootout/run_4algs_generate_random_numbers.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
-echo -n "" > output_rand_nums_spcg.txt
-echo -n "" > output_rand_nums_xoroshiro128plus.txt
-echo -n "" > output_rand_nums_xorshift128plus.txt
 
-gcc spcg64.c -o spcg
+# make a directory to store results
+mkdir results
+declare -a arr=(1000000 5000000 10000000)
+
+#clear the timing results files before we append
+for i in "${arr[@]}"
+do
+    echo -n "" > "results/spcg64-timings-$i-rand-nums.txt"
+    echo -n "" > "results/xoroshiro128plus-timings-$i-rand-nums.txt"
+    echo -n "" > "results/xorshift128plus-timings-$i-rand-nums.txt"
+    echo -n "" > "results/mersennetwister64-timings-$i-rand-nums.txt"
+done
+
+gcc spcg64.c -o spcg64
 gcc xoroshiro128plus.c -o xoroshiro128plus
 gcc xorshift.c -o xorshift128plus
+gcc mersennetwister.c -o mersennetwister64
 
-for run in {1..10}
+for i in "${arr[@]}"
 do
-./spcg >> output_rand_nums_spcg.txt
-./xoroshiro128plus >> output_rand_nums_xoroshiro128plus.txt
-./xorshift128plus >> output_rand_nums_xorshift128plus.txt
+    for run in {1..10}
+    do
+        ./spcg64 $i "results/spcg64-generate-$i-rand-nums.txt" >> "results/spcg64-timings-$i-rand-nums.txt"
+        ./xoroshiro128plus $i "results/xoroshiro128plus-generate-$i-rand-nums.txt" >> "results/xoroshiro128plus-timings-$i-rand-nums.txt"
+        ./xorshift128plus $i "results/xorshift-generate-$i-rand-nums.txt" >> "results/xorshift128plus-timings-$i-rand-nums.txt"
+        ./mersennetwister64 $i "results/mersennetwister64-generate-$i-rand-nums.txt" >> "results/mersennetwister64-timings-$i-rand-nums.txt"
+    done
 done
+
